0f8fb75498220aa91c694fc7921e7eb4e4781276
Glizzus <calcruiseby@gmail.com>
2022-04-27 10:42:18 -0700

    started development for the web application

---
+++ src/main/java/TitleChangesCsvCreator.java
@@ -0,0 +1,33 @@
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class TitleChangesCsvCreator {
+
+    static String url = "https://en.wikipedia.org/wiki/List_of_WWE_Champions";
+
+
+
+
+
+
+
+
+        /*Elements elems = Jsoup.connect(url).get().select("tr").select("td");
+        ArrayList<String> list = new ArrayList<>();
+        for (Element e: elems) {
+            list.add(e.text());
+        }
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }*/
+
+
+}
