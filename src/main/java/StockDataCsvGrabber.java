59d457b47212bf57a543908bf8643515477e9362
Glizzus <calcruiseby@gmail.com>
2022-04-28 15:17:08 -0700

    refactored web scraping classes

--- src/main/java/StockDataCsvGrabber.java
+++ src/main/java/StockDataCsvGrabber.java
@@ -1,3 +1,5 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,7 +10,6 @@ import java.nio.channels.ReadableByteChannel;
 
 import java.text.ParseException;
 import java.text.DateFormat;
-import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
@@ -28,73 +29,22 @@ public class StockDataCsvGrabber {
      * Converts a date to Unix Time.
      *
      * @param dateIn the input date in format yyyy-MM-dd
-     * @return A long that represnts the given date in Unix time
+     * @return A long that represents the given date in Unix time
      */
     private static long dateToUnixTime(String dateIn) throws ParseException {
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         df.setTimeZone(TimeZone.getTimeZone("UTC"));
-        Date date = df.parse(dateIn);
-        return date.getTime() / 1000L;
+        return df.parse(dateIn)
+                .getTime()
+                / 1000L; // converts milliseconds to seconds
     }
-
-    private static String[] getInputs() {
-        String[] inputs = new String[5];
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Stock: " );
-        inputs[0] = scan.nextLine();
-        System.out.print("Start date: ");
-        inputs[1] = scan.nextLine();
-        System.out.print("End date: ");
-        inputs[2] = scan.nextLine();
-        System.out.print("Interval: ");
-        inputs[3] = scan.nextLine();
-        System.out.print("Directory to download to: ");
-        inputs[4] = scan.nextLine();
-        return inputs;
-    }
-
 
     private static Boolean isValidIncrement(String increment) {
-        String[] valids = new String[]{"daily", "weekly", "monthly"};
-        for (String type : valids) {
+        String[] validIntervals = new String[]{"daily", "weekly", "monthly"};
+        for (String type : validIntervals) {
             if (type.equals(increment)) return true;
         }
         return false;
-    }
-
-    /**
-     * Creates a file name for the .csv to be downloaded to.
-     *
-     * @param path      the file path
-     * @param start     the start date in the Gregorian calendar
-     * @param end       the end date in the Gregorian calendar
-     * @param increment the interval of data (daily, weekly, monthly)
-     * @return a String that is the filename of the finished file
-     */
-    private static String makeFileName(String path, String ticker, String start, String end, String increment) {
-        if (!path.endsWith("/")) {
-            path += "/";
-        }
-        else if (!path.endsWith("\\")) {
-            path += "\\";
-        }
-        return path + ticker + "_" + start + "_" + end + "_" + increment;
-    }
-
-    /**
-     * Downloads a file from Yahoo! Finance to a specified directory.
-     *
-     * @param url  the url of the file to download
-     * @param filename  the filename of the file to be downloaded
-     * @throws IOException if the url is incorrect or file is not found
-     */
-    private static void getFileFromYahoo(String url, String filename) throws IOException {
-
-        ReadableByteChannel readChannel = Channels.newChannel(new URL(url).openStream());
-        try (FileOutputStream output = new FileOutputStream(filename)) {
-            output.getChannel()
-                    .transferFrom(readChannel, 0, Long.MAX_VALUE);
-        }
     }
 
     /**
@@ -108,6 +58,7 @@ public class StockDataCsvGrabber {
      * @return A String that is the assembled URL
      */
     private static String buildYahooUrl(String ticker, Long date1, Long date2, String increment) {
+        System.out.println("Creating URL");
         String baseUrl = "https://query1.finance.yahoo.com/v7/finance/download/";
         String period1 = "?period1=" + date1;
         String period2 = "&period2=" + date2;
@@ -121,22 +72,95 @@ public class StockDataCsvGrabber {
         return baseUrl + ticker + period1 + period2 + interval + endUrl;
     }
 
+    /**
+     * Creates a file name for the .csv to be downloaded to.
+     *
+     * @param path      the file path
+     * @param start     the start date in the Gregorian calendar
+     * @param end       the end date in the Gregorian calendar
+     * @param increment the interval of data (daily, weekly, monthly)
+     * @return a String that is the filename of the finished file
+     */
+    private static String makeFileName(String path, String ticker, String start, String end, String increment) {
+        if (!path.endsWith("/")) {
+            path += "/"; // Done for Unix-based directories
+        }
+        else if (!path.endsWith("\\")) {
+            path += "\\"; // Done for Windows directories
+        }
+        return path + ticker + "_" + start + "to" + end + "_" + increment;
+    }
+
+    /**
+     * Downloads a file from any url to a specified directory.
+     *
+     * @param url  the url of the file to download
+     * @param path  the filename of the file to be downloaded
+     * @throws IOException if the url is incorrect or file is not found
+     */
+    private static void getFileFromUrl(String url, String path) throws IOException {
+        System.out.printf("Downloading file from %s%n", url);
+        ReadableByteChannel readChannel = Channels.newChannel(new URL(url).openStream());
+        try (FileOutputStream output = new FileOutputStream(path)) {
+            output.getChannel()
+                    .transferFrom(readChannel, 0, Long.MAX_VALUE);
+        }
+    }
+
+    private static String[] getInputs() {
+        String[] inputs = new String[5];
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Stock: " );
+        inputs[0] = scan.nextLine();
+
+        do {
+            System.out.print("Start date: ");
+            inputs[1] = scan.nextLine();
+            System.out.print("End date: ");
+            inputs[2] = scan.nextLine();
+            if (inputs[2].compareTo(inputs[1]) < 0) System.out.println("End date must come after Start date.");
+        }
+        while (inputs[2].compareTo(inputs[1]) < 0);
+
+        do {
+            System.out.print("Interval: ");
+            inputs[3] = scan.nextLine();
+            if (!isValidIncrement(inputs[3])) System.out.println("Invalid increment, options include\n" +
+                                                                    "[daily], [weekly], [monthly]");
+        }
+        while (!isValidIncrement(inputs[3]));
+
+        Path toCheck;
+        do {
+            System.out.print("Directory to download to: ");
+            inputs[4] = scan.nextLine();
+            toCheck = Path.of(inputs[4]);
+            if (!Files.isDirectory(toCheck)) System.out.println("Directory does not exist.");
+        }
+        while (!Files.isDirectory(toCheck));
+        return inputs;
+    }
+
     private static void getStockDataCsv(String[] args) throws IOException, ParseException {
         if (args.length != 5) throw new IllegalArgumentException();
         String ticker = args[0];
         String start = args[1];
         String end = args[2];
+        if (end.compareTo(start) < 0) throw new IllegalArgumentException("Error: end date must be after start date");
         String increment;
         if (isValidIncrement(args[3])) {
             increment = args[3];
-        } else throw new IllegalArgumentException();
+        }
+        else throw new IllegalArgumentException("Error: Invalid Increment");
         String path = args[4];
+        if (!Files.isDirectory(Path.of(path))) throw new IllegalArgumentException("Error: Directory does not exists");
 
         long unixStart = dateToUnixTime(start);
         long unixEnd = dateToUnixTime(end);
         String url = buildYahooUrl(ticker, unixStart, unixEnd, increment);
-        String filename = makeFileName(path, ticker, start, end, increment);
-        getFileFromYahoo(url, filename);
+        String fullPath = makeFileName(path, ticker, start, end, increment);
+        getFileFromUrl(url, fullPath);
+        System.out.printf("File loaded into %s%n", fullPath);
     }
 
     public static void main(String[] args) throws ParseException, IOException {
@@ -147,6 +171,3 @@ public class StockDataCsvGrabber {
         }
     }
 }
-
-
-
---
+++ src/main/java/WWERatingsCsvGrabber.java
@@ -0,0 +1,173 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/** This aids in gathering WWE ratings data from www.wrestlingdata.com.
+ *
+ * It employs multithreading to web scrape the aforementioned website and creates a csv file for use elsewhere.
+ *
+ * @author Cal Crosby
+ */
+public final class WWERatingsCsvGrabber {
+
+
+    private WWERatingsCsvGrabber() {} // don't let anybody instantiate this class
+
+
+     private Document tryGetDocument(String urlIn) {
+        try {
+            return Jsoup.connect(urlIn).get();
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Could not retrieve the webpage");
+         }
+    }
+
+    private static class ScrapeResults {
+        ArrayList<String> dates = new ArrayList<>();
+        ArrayList<String> shows = new ArrayList<>();
+        ArrayList<String> relRatings = new ArrayList<>();
+        ArrayList<String> absRatings = new ArrayList<>();
+
+        /**
+         * Formats ScrapeResults into a csv String.
+         *
+         * @param i an integer representing the index of the ScrapeResults to use
+         * @return a formatting String to be added to a .csv file
+         */
+        private String resultFormat(int i, int page) {
+            // Ensures there is no newline for the last line of a .csv file
+            String form = (i == 0 && page == 1) ? "%s,%s,%s,%s" : "%s,%s,%s,%s\n";
+            return String.format(form,
+                    dates.get(i).replace('/', '-'),
+                    shows.get(i),
+                    relRatings.get(i),
+                    absRatings.get(i).replaceAll(",", ""));
+        }
+    }
+
+    private class Scraper implements Runnable {
+          Thread thread;
+          ScrapeResults results = new ScrapeResults();
+          String toScrape;
+          String url;
+          Scraper(String whatToScrape, String urlIn) {
+              toScrape = whatToScrape;
+              url = urlIn;
+        }
+        public void run() {
+            System.out.println("Scraping " + toScrape);
+              Document doc = tryGetDocument(url);
+            Elements elems;
+            switch (toScrape) {
+                case "dates" -> {
+                    elems = doc.select("table")
+                            .select("td[align=center][bgcolor=#660000][style=with:20%;]");
+                    for (Element e : elems) {
+                        System.out.println(e.text());
+                        results.dates.add(e.text());
+                    }
+                }
+                case "shows" -> {
+                    elems = doc.select("table").select("div[title=Show event]");
+                    for (Element e : elems) {
+                        System.out.println(e.text());
+                        results.shows.add(e.text());
+                    }
+                }
+                case "ratings" -> {
+                    elems = doc.select("table").select("td[style=width:13%;][bgcolor=#660000]");
+                    for (int i = 0; i < elems.size(); i += 2) {
+                        System.out.println(elems.get(i).text());
+                        results.relRatings.add(elems.get(i).text());
+                    }
+                    for (int i = 1; i < elems.size(); i += 2) {
+                        System.out.println(elems.get(i).text());
+                        results.absRatings.add(elems.get(i).text());
+                    }
+                }
+                default -> throw new IllegalArgumentException();
+            }
+        }
+        private void start() {
+              thread = new Thread(this);
+              thread.start();
+        }
+        private void join() throws InterruptedException {
+              this.thread.join();
+        }
+        private ScrapeResults scrape() throws InterruptedException {
+              start();
+              join();
+              return results;
+        }
+    }
+
+    private ScrapeResults scrapePage(String url) throws InterruptedException {
+        Scraper dateScraper = new Scraper("dates", url);
+        Scraper showScraper = new Scraper("shows", url);
+        Scraper ratingsScraper = new Scraper("ratings", url);
+
+        ScrapeResults results = new ScrapeResults();
+        results.dates = dateScraper.scrape().dates;
+        results.shows = showScraper.scrape().shows;
+
+        ScrapeResults ratings = ratingsScraper.scrape();
+        results.relRatings = ratings.relRatings;
+        results.absRatings = ratings.absRatings;
+
+        dateScraper.join();
+        showScraper.join();
+        ratingsScraper.join();
+
+        return results;
+    }
+
+
+    /**
+     * This creates a new .csv file, and adds the data to it starting from oldest to newest.
+     * @throws InterruptedException if the thread is interrupted
+     * @throws IOException if the file cannot be opened
+     */
+    private void writeFile() throws InterruptedException, IOException {
+
+        try (FileWriter f = new FileWriter("src/Resources/main_ratings.csv")) {
+            f.write("Date,Event,Relative Rating, Absolute Rating\n");
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Failed to create new file");
+        }
+        int pageCount = 69;
+        for (int page = pageCount; page > 0; page--) {
+
+            // This is the base of the url that contains the data. Each page number is tacked on at the end.
+            String baseUrl = "https://www.wrestlingdata.com/index.php?befehl=quoten&art=2&liga=1&show=&sort=0&seite=";
+            String url = baseUrl + page;
+            ScrapeResults results = scrapePage(url);
+
+            FileWriter csv = null;
+            try {
+                csv = new FileWriter("src/Resources/main_ratings.csv", true);
+                for (int i = results.dates.size() - 1; i >= 0; i--) {
+                    csv.write(results.resultFormat(i, page));
+                }
+            }
+            catch (IOException e) {
+                throw new RuntimeException("Failed to write to file");
+            }
+            finally {
+                assert csv != null;
+                csv.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new WWERatingsCsvGrabber().writeFile();
+    }
+}
